93fc02c345478da9908ece2f81e396a51d8eff62
broutage26 <hopeture_30@pm.me>
2026-03-12 12:40:48 +0100

    Preparation

---
+++ src/CompagnieAerienne/Avion.java
@@ -0,0 +1,14 @@
+package CompagnieAerienne;
+
+public class Avion {
+    public static String AfficherDetails(){
+        int id;
+        String modele;
+        int capacite;
+        String compagnie;
+        float anneeService;
+        //Siege[] sieges;
+
+        return "a";
+    }
+}
---
+++ src/CompagnieAerienne/Vol.java
@@ -0,0 +1,25 @@
+package CompagnieAerienne;
+
+public class Vol {
+
+
+
+
+    public static String afficherDetails() {
+        int numeroVol;
+        int numeroDepart;
+        String villeDepart;
+        String villeDestination;
+        double dateDepart;
+        Avion avion;
+
+        return "a";
+    }
+
+    public static boolean reserverPlace(){
+
+        int placesDisponibles;
+        return true;
+    }
+
+}
